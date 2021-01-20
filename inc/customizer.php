e8fac23264dcd4ec6ad7177bf1aad4273e6224fa
Ali Lopez <zero.exu@gmail.com>
2021-01-19 22:44:53 -0300

    init dark theme

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * ze2021 Theme Customizer
+ *
+ * @package ze2021
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function ze2021_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'ze2021_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'ze2021_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'ze2021_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function ze2021_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function ze2021_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function ze2021_customize_preview_js() {
+	wp_enqueue_script( 'ze2021-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'ze2021_customize_preview_js' );
